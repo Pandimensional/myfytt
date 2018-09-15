056034687616684380028da868985a18d57ea23d
khnshr <shehryark@gmail.com>
2018-09-15 10:45:08 +0200

    Stylekit

---
+++ myfytt/myview.swift
@@ -0,0 +1,19 @@
+//
+//  myview.swift
+//  myfytt
+//
+//  Created by Shehryar K on 2018-09-15.
+//  Copyright © 2018 Shehryar K. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class myview: UIView {
+    
+    // Swift
+    override func draw(_ rect: CGRect){
+        StyleKit.drawPeppAppSignUpPage()
+    }
+
+}
